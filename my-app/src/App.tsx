dab3960a9994f9d4b1e3c06ac5f00caf57395c65
Michael Eifert (ext.) <Michael.Eifert@partner.bmwgroup.com>
2022-12-23 19:21:54 +0100

    Add navigation

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-function App() {
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Group from './pages/Group';
+import Expenses from './pages/Expenses';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/expenses" element={<Expenses/>}/>
+          <Route path="/group" element={<Group/>}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
