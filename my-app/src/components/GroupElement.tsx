27bbb2237cacbb4c9afeaaea206e0c8eb374ed04
Michael Eifert <eifert.michael.1994@gmail.com>
2023-04-15 08:36:42 +0200

    Add some styling to the group element

--- my-app/src/components/GroupElement.tsx
+++ my-app/src/components/GroupElement.tsx
@@ -1,9 +1,9 @@
-// src/components/GroupElement.tsx
 import React from 'react'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import { Group } from '../model/Group'
+import { Button, CardActions } from '@mui/material'
 
 interface Props {
   group: Group
@@ -11,11 +11,28 @@ interface Props {
 
 const GroupElement: React.FC<Props> = ({ group }) => {
   return (
-    <Card sx={{ minWidth: 275, marginBottom: 2 }}>
+    <Card
+      sx={{
+        minWidth: 275,
+        mt: 5,
+        bgcolor: 'rgba(190, 190, 190, 0.308)',
+      }}
+    >
       <CardContent>
         <Typography variant="h5" component="div">
           {group.name}
         </Typography>
+        <CardActions
+          sx={{
+            mt: '10px',
+            display: 'flex', // Use flexbox
+            justifyContent: 'center', // Center the content horizontally
+          }}
+        >
+          <Button size="small" variant="contained">
+            Details
+          </Button>
+        </CardActions>
       </CardContent>
     </Card>
   )
